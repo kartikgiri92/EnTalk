de6f178aae612b219c10970ba435fbec95a8ca8e
kartikgiri92 <kartikgiri92>
2019-10-07 18:01:15 +0530

    m

---
+++ profiles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Profile():
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+
+    token = models.CharField(blank=True, max_length=30)
+
+    time_token_created = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return(self.user.email)
