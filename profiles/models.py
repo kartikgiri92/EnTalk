85bce65de6627416cd69feb3a06be8748fe33100
Kartik Giri <kartikgiri92@gmail.com>
2020-01-12 21:13:41 +0530

    a

--- profiles/models.py
+++ profiles/models.py
@@ -5,12 +5,10 @@ User = get_user_model()
 
 class Profile():
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    identification_num = models.IntegerField()
-
     token = models.CharField(blank=True, max_length=30)
-
-    time_token_created = models.CharField(blank=True, max_length=20)
+    time_token_created = models.CharField(blank=True, max_length=30)
+    private_key = models.CharField(max_length=50)
+    public_key = models.CharField(max_length=50)
 
     def __str__(self):
-        return(self.user.email)
+        return(self.user.username)
