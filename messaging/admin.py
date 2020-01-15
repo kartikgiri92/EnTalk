83f21c0d930f180879ff81cdfeb4014a0538ebcb
Kartik Giri <kartikgiri92@gmail.com>
2020-01-15 19:26:24 +0530

    Message Model added

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+import messaging.models as mssg_models
 
 # Register your models here.
+admin.site.register(mssg_models.Message)
--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name = "sender", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name = "receiver", on_delete=models.CASCADE)
+    mssg = models.TextField(default='')
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(self.sender.username)
